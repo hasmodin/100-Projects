686e1fa234089be5ed82f95424806012b5fe4ec0
Hasmodin <hasmodin@gmail.com>
2024-06-06 09:39:51 +0300

    update files

---
+++ 13.Page Scroll Indicator HTML CSS JS/app.js
@@ -0,0 +1,8 @@
+let indicator = document.querySelector(".scroll-indicator .progressbar");
+let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+window.addEventListener("scroll", scroll);
+function scroll() {
+    let scrollTop = document.documentElement.scrollTop;
+    let scrolled = (scrollTop / scrollHeight) * 100;
+    indicator.style.width = `${scrolled}%`
+}
